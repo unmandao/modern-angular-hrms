0afa737f0e12a8ba3ac92b4e19de34fd6d09c9f9
dbausela <dbausela@gmail.com>
2025-11-19 08:56:35 +0100

    pg. 8

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,9 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   imports: [],
-  templateUrl: './login.html',
+  template: `
+  <div class="login-container">
+      <h1>Login</h1>
+      <form>
+          <input type="text" name="email" placeholder="Email">
+          <input type="password" name="password"
+              placeholder="Password">
+          <button type="submit">Login</button>
+      </form>
+  </div>
+  `,  
   styleUrl: './login.css',
 })
 export class Login {
 
+  credentials = { email: '', password: '' };
+  
 }
